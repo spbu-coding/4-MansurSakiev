083db8632861066c6da50296863e9b66afde3475
Mansur <71905982+MansurSakiev@users.noreply.github.com>
2020-12-07 14:19:10 +0500

    Update CONVERTER.c

--- src/CONVERTER.c
+++ src/CONVERTER.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "BMP.h"
-#include "QDBMP.h"
+#include "bmp.h"
+#include "qdbmp.h"
 
 #define NORMAL_ARGUMENTS_COUNT 3
 #define error(...) (fprintf(stderr, __VA_ARGS__))
